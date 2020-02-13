598db1d238275c1fbbfd5edff71a686793f4a634
dengjunxing <dengjunxing@leagsoft.com>
2020-02-13 16:08:24 +0800

    openssl 相关测试代码

---
+++ normalopenssl-test/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "testso.h"
+int main()
+{
+	show();
+	printf("hello world\n");
+	return 0;
+}
---
+++ normalopenssl-test/testso.h
@@ -0,0 +1,5 @@
+#ifndef __TEST_SO_H__
+#define __TEST_SO_H__
+void show();
+
+#endif
